54a2e547558b87d0d1ee54acba3b0770be748496
ElizabethSh <liza.shoshkina@gmail.com>
2021-01-09 16:49:21 +0100

    Урок 46:Создание главной страницы: Quiz и ActiveQuiz

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import { Component } from 'react';
-import Layout from './hoc/Layout/Layout'
+import Layout from './hoc/Layout/Layout';
+import Quiz from './containers/Quiz/Quiz';
 
 class App extends Component {
   render() {
     return (
         <Layout>
-          <div style={{width: `400px`, border: `1px solid black`}}>
-            <h1>Layout works</h1>
-          </div>
+          <Quiz />
         </Layout>
     );
   }
---
+++ src/containers/Quiz/Quiz.js
@@ -0,0 +1,18 @@
+import { Component } from "react";
+import './Quiz.css';
+
+class Quiz extends Component {
+  state={
+    quiz: []
+  }
+
+  render() {
+    return (
+      <div className='Quiz'>
+        <h1>Quiz</h1>
+      </div>
+    )
+  }
+}
+
+export default Quiz;
