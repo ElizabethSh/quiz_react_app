d29dab3e2ac87e3da50df74b6071d668491865bf
ElizabethSh <liza.shoshkina@gmail.com>
2021-01-12 16:36:02 +0100

    Урок 54:Подсчет правильных ответов

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -2,22 +2,38 @@ import React from 'react';
 import './FinishedQuiz.css';
 
 const FinishedQuiz = props => {
+  const {results, restartHandler, quiz} = props;
+
+  const answers = Object.values(results); // получаем массив с ответами без ключей
+  const correctAnswers = answers.reduce((acc, answer) => {  // считаем сколько из них правильные
+    if (answer === `success`) {
+      acc ++;
+    }
+    return acc;
+  }, 0)
+
   return (
     <div className='FinishedQuiz'>
       <ul>
-        <li>
-          <b>1.</b>
-          How are you?
-          <i className={'fa fa-times error'}/>
-        </li>
-        <li>
-          <b>2.</b>
-          How are you?
-          <i className={'fa fa-check success'}/>
-        </li>
+        {quiz.map((quizItem, index) => {
+
+          // формируем имя класса для отображения и оформления иконок
+          const classes = [`fa`,  // обязательный префикс
+          results[quizItem.id] === `error` ? `fa-times` : `fa-check`, // крестик или галка
+          results[quizItem.id]  // добавление класса success или error
+          ];
+
+          return (
+            <li key={index}>
+              <b>{index + 1}</b>.&nbsp;
+              {quizItem.question}
+              <i className={classes.join(` `)}/>
+            </li>
+          )
+        })}
       </ul>
-      <p>Correct answers are 4 out of 10</p>
-      <button>Start again</button>
+      <p>Correct answers are {correctAnswers} out of {quiz.length}</p>
+      <button onClick={restartHandler}>Restart</button>
     </div>
   )
 }
--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -5,30 +5,31 @@ import FinishedQuiz from '../../components/FinishedQuiz/FinishedQuiz';
 
 class Quiz extends Component {
   state = {
-    isQuizFinished: true,
+    results: {}, // 1. заводим пустой объект, далее будет объект типа {[questionId]: `success` or `error`}
+    isQuizFinished: false,
     currentQuestion: 0,
     answerState: null, // далее будет объект типа {[answerId]: `success` or `error`}
     quiz: [
       {
-        id: `01`,
+        id: 1,
         question: `What color is the grass?`,
-        correctAnswer: `3`,
+        correctAnswer: 3,
         answers: [
-          {text: `Black`, id: `1`},
-          {text: `Yellow`, id: `2`},
-          {text: `Green`, id: `3`},
-          {text: `Blue`, id: `4`},
+          {text: `Black`, id: 1},
+          {text: `Yellow`, id: 2},
+          {text: `Green`, id: 3},
+          {text: `Blue`, id: 4},
         ]
       },
       {
-        id: `02`,
+        id: 2,
         question: `What time of year is it now?`,
-        correctAnswer: `1`,
+        correctAnswer: 1,
         answers: [
-          {text: `Winter`, id: `1`},
-          {text: `Spring`, id: `2`},
-          {text: `Summer`, id: `3`},
-          {text: `Autumn`, id: `4`},
+          {text: `Winter`, id: 1},
+          {text: `Spring`, id: 2},
+          {text: `Summer`, id: 3},
+          {text: `Autumn`, id: 4},
         ]
       },
     ]
@@ -46,10 +47,19 @@ class Quiz extends Component {
     }
 
     const question = this.state.quiz[this.state.currentQuestion]; // получаем объект текущего вопроса
+    const results = this.state.results; // 2. Достаем из стейта объект с результатами
 
     if (question.correctAnswer === answerId) {
+
+      // 4. Проверяем, есть ли в объекте results с таким ключом какое-то значение:
+      // если есть, то ничего не записываем (значит ответ был неверным и этот результат нужно сохранить)
+      // если нет, значит ответ сразу был правильным и этот результат нужно записать
+      if (!results[question.id]) {
+        results[question.id] = `success`;
+      }
       this.setState({
-        answerState: {[answerId]: `success`}
+        answerState: {[answerId]: `success`},
+        results
       })
 
       const timeout = window.setTimeout(() => {
@@ -64,10 +74,31 @@ class Quiz extends Component {
         window.clearTimeout(timeout);
       }, 1000)
     } else {
+
+      // 3. если ответ неправильный, то добавляем в объект
+      // ключ с номером вопроса и результат ответа `error`
+      // ------------------------------------------------
+      // ВнИМАНИЕ! Сначала обновляем переменную result, в которую
+      // сохранили this.state.result, а затем уже
+      // в setState добавляем обновленное значение result
+      // c ответом пользователя!
+      results[question.id] = `error`;
       this.setState({
-        answerState: {[answerId]: `error`}
+        answerState: {[answerId]: `error`},
+        results
       })
     }
+  }
+
+  // сбрасывает стейт до изначального
+  // таким образом опрос начинается заново
+  restartHandler = () => {
+    this.setState({
+      isQuizFinished: false,
+      currentQuestion: 0,
+      answerState: null,
+      results: {},
+    });
   }
 
   // проверяет, есть ли еще вопросы в опроснике
@@ -81,8 +112,12 @@ class Quiz extends Component {
         <div className="Quiz__wrapper">
           <h1>Answer the questions</h1>
           {this.state.isQuizFinished
-          ? <FinishedQuiz />
-          : <ActiveQuiz 
+          ? <FinishedQuiz
+            quiz={this.state.quiz}
+            results={this.state.results}  //5. передаем результат ответов на финальный экран
+            restartHandler={this.restartHandler}
+          />
+          : <ActiveQuiz
             answers={this.state.quiz[this.state.currentQuestion].answers}
             question={this.state.quiz[this.state.currentQuestion].question}
             answerClickHandler={this.answerClickHandler}
@@ -90,7 +125,7 @@ class Quiz extends Component {
             currentQuestion={this.state.currentQuestion + 1}
             answerState={this.state.answerState}
           />
-        }  
+        }
         </div>
       </div>
     )
