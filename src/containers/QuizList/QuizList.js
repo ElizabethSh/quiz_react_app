12d5c01b72b41f14d75c876646f5e8e8278a2de6
ElizabethSh <liza.shoshkina@gmail.com>
2021-01-18 20:06:37 +0100

    Урок 72: Создаем страницу списка опросов

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,10 +1,35 @@
 import {React, Component} from 'react';
+import {NavLink} from 'react-router-dom';
+import './QuizList.css';
+
+const quizList = [1, 2, 3];
 
 class QuizList extends Component {
+
+  // рендерит элементы списка с ссылками на опросы
+  renderQuizes() {
+    return (
+      quizList.map((quizItem, index) => {
+        return (
+          <li key={index}>
+            <NavLink
+              to={'/quiz/' + quizItem} // динамический адрес - например /quiz/1
+            >Quiz {quizItem}</NavLink>
+          </li>
+        )
+      })
+    )
+  }
+
   render() {
     return(
-      <div>
-        <h1>QuizList</h1>
+      <div className='QuizList'>
+        <div>
+          <h1>Quiz List</h1>
+          <ul>
+            {this.renderQuizes()}
+          </ul>
+        </div>
       </div>
     )
   }
