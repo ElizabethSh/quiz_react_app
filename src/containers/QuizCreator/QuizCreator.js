846be58d0d3a3f1244788ee101590b27dc74c27e
ElizabethSh <liza.shoshkina@gmail.com>
2021-01-20 14:35:02 +0100

    Урок 79:Создание страницы добавления опроса

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,10 +1,48 @@
 import {React, Component} from 'react';
+import './QuizCreator.css';
+import Button from '../../components/UI/Button/Button';
 
 class QuizCreator extends Component {
+  submitHandler = (e) => {
+    e.preventDefault();
+  }
+
+  addQuestionHandler = () => {
+
+  }
+
+  addQuizHandler = () => {
+
+  }
+
   render() {
     return(
-      <div>
-        <h1>QuizCreator</h1>
+      <div className={'QuizCreator'}>
+        <div>
+          <h1>Add Quiz</h1>
+          <form onSubmit={this.submitHandler}>
+            <input />
+            <hr />
+            <input />
+            <input />
+            <input />
+            <input />
+
+            <select />
+
+          <Button 
+            type='primary' 
+            onClick={this.addQuestionHandler}
+          >Add question</Button>
+
+          <Button 
+            type='success' 
+            onClick={this.addQuizHandler}
+          >Add quiz</Button>
+
+          </form>
+
+        </div>
       </div>
     )
   }
