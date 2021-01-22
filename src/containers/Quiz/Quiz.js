85b484d17c8ab2529e78bf6547fe937bbdc54111
ElizabethSh <liza.shoshkina@gmail.com>
2021-01-22 20:10:21 +0100

    Удаляет комментарии

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -5,10 +5,10 @@ import FinishedQuiz from '../../components/FinishedQuiz/FinishedQuiz';
 
 class Quiz extends Component {
   state = {
-    results: {}, // 1. заводим пустой объект, далее будет объект типа {[questionId]: `success` or `error`}
+    results: {},
     isQuizFinished: false,
     currentQuestion: 0,
-    answerState: null, // далее будет объект типа {[answerId]: `success` or `error`}
+    answerState: null,
     quiz: [
       {
         id: 1,
@@ -35,8 +35,6 @@ class Quiz extends Component {
     ]
   }
 
-  // СТРЕЛОЧНАЯ функция! - контекст не будет теряться!!!
-  // Если завести обычную - то нужно биндить в конструкторе!
   answerClickHandler = (answerId) => {
     if (this.state.answerState) {
       const key = Object.keys(this.state.answerState)[0];
@@ -46,14 +44,10 @@ class Quiz extends Component {
       }
     }
 
-    const question = this.state.quiz[this.state.currentQuestion]; // получаем объект текущего вопроса
-    const results = this.state.results; // 2. Достаем из стейта объект с результатами
+    const question = this.state.quiz[this.state.currentQuestion];
+    const results = this.state.results;
 
     if (question.correctAnswer === answerId) {
-
-      // 4. Проверяем, есть ли в объекте results с таким ключом какое-то значение:
-      // если есть, то ничего не записываем (значит ответ был неверным и этот результат нужно сохранить)
-      // если нет, значит ответ сразу был правильным и этот результат нужно записать
       if (!results[question.id]) {
         results[question.id] = `success`;
       }
@@ -74,14 +68,6 @@ class Quiz extends Component {
         window.clearTimeout(timeout);
       }, 1000)
     } else {
-
-      // 3. если ответ неправильный, то добавляем в объект
-      // ключ с номером вопроса и результат ответа `error`
-      // ------------------------------------------------
-      // ВнИМАНИЕ! Сначала обновляем переменную result, в которую
-      // сохранили this.state.result, а затем уже
-      // в setState добавляем обновленное значение result
-      // c ответом пользователя!
       results[question.id] = `error`;
       this.setState({
         answerState: {[answerId]: `error`},
@@ -107,7 +93,7 @@ class Quiz extends Component {
   }
 
   componentDidMount() {
-    console.log(this.props.match.params.id); // выводим id опроса из адресной строки
+    console.log(this.props.match.params.id);
   }
 
   render() {
@@ -116,20 +102,20 @@ class Quiz extends Component {
         <div className="Quiz__wrapper">
           <h1>Answer the questions</h1>
           {this.state.isQuizFinished
-          ? <FinishedQuiz
-            quiz={this.state.quiz}
-            results={this.state.results}  //5. передаем результат ответов на финальный экран
-            restartHandler={this.restartHandler}
-          />
-          : <ActiveQuiz
-            answers={this.state.quiz[this.state.currentQuestion].answers}
-            question={this.state.quiz[this.state.currentQuestion].question}
-            answerClickHandler={this.answerClickHandler}
-            questionAmount={this.state.quiz.length}
-            currentQuestion={this.state.currentQuestion + 1}
-            answerState={this.state.answerState}
-          />
-        }
+            ? <FinishedQuiz
+              quiz={this.state.quiz}
+              results={this.state.results}
+              restartHandler={this.restartHandler}
+            />
+            : <ActiveQuiz
+              answers={this.state.quiz[this.state.currentQuestion].answers}
+              question={this.state.quiz[this.state.currentQuestion].question}
+              answerClickHandler={this.answerClickHandler}
+              questionAmount={this.state.quiz.length}
+              currentQuestion={this.state.currentQuestion + 1}
+              answerState={this.state.answerState}
+            />
+          }
         </div>
       </div>
     )
